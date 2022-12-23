8ae669f3b2823fea357217aa94ddf93f4238b28d
Павел Корчилов <pencool2@yandex.ru>
2022-12-24 00:03:03 +0300

    накидал модели для каталога, добавил приложение в settings, добавил модель в админку.

---
+++ antalia_project/flats/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Images, Flat
+
+
+class ImagesLine(admin.TabularInline):
+    fk_name = 'flat'
+    model = Images
+
+
+@admin.register(Flat)
+class FlatAdmin(admin.ModelAdmin):
+    inlines = [ImagesLine, ]
---
+++ antalia_project/flats/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+STATUS_CHOICES = ((1, 'Новостройка'), (2, 'Вторичное'))
+AREA_CHOICES = ((1, 'Анталия'), (2, 'Стамбул'), (3, 'Северный Кипр'))
+TYPE_CHOICES = ((1, 'Участок'), (2, 'Студия'), (3, 'Квартира'), (4, 'Вилла'),)
+ROOM_CHOICES = ((1, 0), (2, 1), (3, 2), (4, 3), (5, 4))
+
+
+class Flat(models.Model):
+    title = models.CharField(max_length=250)
+    price = models.IntegerField()
+    area = models.IntegerField()
+    floor = models.IntegerField()
+    year = models.IntegerField()
+    desc = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Rent(models.Model):
+    area = models.CharField(max_length=50, choices=AREA_CHOICES)
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES)
+    room = models.IntegerField(choices=ROOM_CHOICES)
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE,
+                             related_name='rent')
+
+
+class Buy(models.Model):
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES)
+    area = models.CharField(max_length=50, choices=AREA_CHOICES)
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES)
+    room = models.IntegerField(choices=ROOM_CHOICES)
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE,
+                             related_name='buy')
+
+
+class Images(models.Model):
+    image = models.ImageField(
+        upload_to='flats/images/', null=True, default=None)
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE,
+                             related_name='images')
