43c6f0122ab331cd0725be8fb8632d669d24ef1e
Roman Petrakov <development@telfia.com>
2022-12-27 21:03:58 +0300

    добавляет приложение about и модель избранного

--- catalog/models.py
+++ catalog/models.py
@@ -29,6 +29,7 @@ class Location(models.Model):
 
 class PropertyType(models.Model):
     """Модель типа недвижимости"""
+
     name = models.CharField(max_length=50)
 
     class Meta:
@@ -41,6 +42,7 @@ class PropertyType(models.Model):
 
 class Facility(models.Model):
     """Модель удобств"""
+
     name = models.CharField(max_length=100)
     icon = models.CharField(max_length=100)
 
@@ -54,6 +56,7 @@ class Facility(models.Model):
 
 class RentSell(models.Model):
     """М2М Модель для аренды/продажи"""
+
     rent_or_sell = models.IntegerField(
         choices=RENT_SELL_CHOICES,
         default=1,
@@ -65,6 +68,7 @@ class RentSell(models.Model):
 
 class Images(models.Model):
     """М2М Модель для фотографий объекта"""
+
     image = models.ImageField(upload_to='estate/images/', verbose_name='Фото')
 
     class Meta:
@@ -95,15 +99,20 @@ class Object(models.Model):
     description = models.TextField()
     period = models.IntegerField(choices=PERIOD_CHOICES, default=1)
     location = models.ForeignKey(
-        Location, on_delete=models.SET_NULL, null=True
+        Location, on_delete=models.SET_NULL, null=True, related_name='objects'
     )
     type = models.ForeignKey(
-        PropertyType, on_delete=models.SET_NULL, null=True
+        PropertyType,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='objects',
     )
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    facility = models.ManyToManyField(Facility)
-    rent_or_sell = models.ManyToManyField(RentSell)
-    image = models.ManyToManyField(Images)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='objects'
+    )
+    facility = models.ManyToManyField(Facility, related_name='objects')
+    rent_or_sell = models.ManyToManyField(RentSell, related_name='objects')
+    image = models.ManyToManyField(Images, related_name='objects')
 
     class Meta:
         verbose_name = 'Объект'
@@ -111,8 +120,27 @@ class Object(models.Model):
 
     def __str__(self):
         return self.MODEL_STRING.format(
-            name=self.name,
+            name=self.title,
             location=self.location,
             type=self.type,
             rent_or_sell=self.rent_or_sell,
+        )
+
+
+class Favorite(models.Model):
+    """Модель избранного"""
+
+    MODEL_STRING = 'Избранный объект {objects.:30} пользователя {user}'
+
+    object = models.ForeignKey(
+        Object, on_delete=models.CASCADE, related_name='favorites'
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='favorites'
+    )
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.MODEL_STRING.format(
+            object=self.object, user=self.user.get_username()
         )
