b64b6f3d3e997b6e7d7002bd9fea98354185030c
Roman Petrakov <development@telfia.com>
2023-01-29 12:21:48 +0300

    фиксит REQUIRED_FIELDS в модели юзеров

--- users/models.py
+++ users/models.py
@@ -36,7 +36,7 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
