df2fef302142313e987a7eee20283e80e1402d13
Roman Petrakov <development@telfia.com>
2023-02-05 01:45:18 +0300

    #44

--- antalia_project/settings.py
+++ antalia_project/settings.py
@@ -140,7 +140,7 @@ REST_FRAMEWORK = {
     ],
 }
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
