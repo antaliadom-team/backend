bcfb71b05a930270199c4d56c3ce83249bed9932
Павел Корчилов <pencool2@yandex.ru>
2023-02-09 19:52:58 +0300

    раскидал константы и настройки внешних модулей по отдельным файликам

---
+++ antalia_project/config.py
@@ -0,0 +1,71 @@
+from datetime import timedelta
+
+# настройки валидации паролей
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 7},
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'
+    },
+]
+
+# настройки rest-framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+}
+
+# настройки simple-jwt
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+}
+
+# настройки Djoser
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    'SEND_ACTIVATION_EMAIL': False,
+    'HIDE_USERS': True,
+    'USER_CREATE_PASSWORD_RETYPE': True,
+    'SET_PASSWORD_RETYPE': True,
+    'TOKEN_MODEL': None,
+    'PERMISSIONS': {
+        'user': ['djoser.permissions.CurrentUserOrAdmin'],
+        'user_create': ['rest_framework.permissions.AllowAny'],
+        'user_list': ['djoser.permissions.CurrentUserOrAdmin'],
+    },
+    'SERIALIZERS': {
+        'user': 'api.serializers.user_serializers.UserSerializer',
+        'current_user': 'api.serializers.user_serializers.UserSerializer',
+        'user_create_password_retype': 'api.serializers.user_serializers.RegisterUserSerializer',
+    },
+}
+
+# настройки swagger
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'JWT [Bearer {JWT}]': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+        }
+    }
+}
---
+++ antalia_project/constants.py
@@ -0,0 +1,58 @@
+import os
+# В файле .env хранятся имя юзера и пароль приложения
+#константы для отправки электронных писем
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_REPLY_TO = 'antalyadom@telfia.com'
+EMAIL_ADMIN_MESSAGE = (
+    'Поступила заявка от {user_full_name}.'
+    'Были предоставлены следующие данные:'
+    'Имя: {first_name}'
+    'Фамилия: {last_name}'
+    'Электронная почта: {email}'
+    'Тип сделки: {category}'
+    'Локация: {location}'
+    'Тип недвижимости: {property_type}'
+    'Количество комнат: {rooms}'
+    'Номер для связи: {phone_number}'
+    'Дата подачи заявки: {date_added}'
+    'Комментарий: {comment}'
+)
+EMAIL_USER_MESSAGE = (
+    'Здравствуйте {user_full_name}. Ваша заявка была принята в работу,'
+    'скоро с вами свяжется сотрудник нашего агентства.'
+    'По вашей заявке были приняты следующие данные:'
+    'Имя: {first_name}'
+    'Фамилия: {last_name}'
+    'Электронная почта: {email}'
+    'Тип сделки: {category}'
+    'Локация: {location}'
+    'Тип недвижимости: {property_type}'
+    'Количество комнат: {rooms}'
+    'Номер для связи: {phone_number}'
+    'Дата подачи заявки: {date_added}'
+    'Комментарий: {comment}'
+)
+EMAIL_HTML_MESSAGE_ADMIN = ''  # можно прикрутить HTML
+EMAIL_HTML_MESSAGE_USER = ''  # можно прикрутить HTML
+
+# Константы для моделей
+EMAIL_LENGTH = 50
+USER_ROLE_LENGTH = 6
+PHONE_LENGTH = 14
+NAMES_LENGTH = 30
+LONG_NAMES_LENGTH = 100
+ESTATE_TITLE_LENGTH = 200
+SLUG_LENGTH = 100
+LONG_SLUG_LENGTH = 255
+PROPERTY_MAX_LENGTH = 50
+ICON_SLUG = 100
+COMMENT_LENGTH = 200
+CONF_CODE_LENGTH = 32
+PASSWORD_LENGTH = 128
+IMAGE_LIMIT = 6
