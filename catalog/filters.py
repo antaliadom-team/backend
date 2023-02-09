fb1e5a8b79e1a4429ccf7423222220cc5890a6df
Roman Petrakov <development@telfia.com>
2023-02-10 01:56:25 +0300

    small fix to get_favortie and docstrings

--- catalog/filters.py
+++ catalog/filters.py
@@ -4,26 +4,32 @@ from catalog.models import RealEstate
 
 
 class CharFilterInFilter(filters.BaseInFilter, filters.CharFilter):
-    pass
+    """Фильтр для поиска по списку значений."""
 
 
 class RealEstateFilter(filters.FilterSet):
-    category = CharFilterInFilter(
-        field_name='category__id', lookup_expr='in')
+    """Фильтр недвижимости."""
+
+    category = CharFilterInFilter(field_name='category__id', lookup_expr='in')
     property_type = CharFilterInFilter(
-        field_name='property_type__id', lookup_expr='in')
+        field_name='property_type__id', lookup_expr='in'
+    )
     location = CharFilterInFilter(field_name='location__id', lookup_expr='in')
     rooms = filters.NumberFilter(method='four_and_more')
     is_favorited = filters.BooleanFilter(method='get_favorite')
 
     def get_favorite(self, queryset, name, value):
-        if value:
+        """Возвращает избранные объекты недвижимости."""
+        if value and self.request.user.is_authenticated:
             return self.queryset.filter(
-                id__in=self.request.user.favorites.all().values_list(
-                    'real_estate_id', flat=True).order_by('-date_added'))
+                id__in=self.request.user.favorites.all()
+                .values_list('real_estate_id', flat=True)
+                .order_by('-date_added')
+            )
         return queryset
 
     def four_and_more(self, queryset, name, value):
+        """Возвращает объекты недвижимости с количеством комнат"""
         if 0 < value < 4:
             return self.queryset.filter(rooms=value)
         return self.queryset.filter(rooms__gte=4)
@@ -31,5 +37,9 @@ class RealEstateFilter(filters.FilterSet):
     class Meta:
         model = RealEstate
         fields = [
-            'category', 'property_type', 'location', 'rooms', 'is_favorited'
+            'category',
+            'property_type',
+            'location',
+            'rooms',
+            'is_favorited',
         ]
