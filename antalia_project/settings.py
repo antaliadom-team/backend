e6e57e2f97caaf12d5748b7c76f737b6c5e1f9f9
Roman Petrakov <development@telfia.com>
2023-02-12 23:56:56 +0300

    фикс 500 ошибки по адресу /api/

--- antalia_project/settings.py
+++ antalia_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'djoser',
@@ -49,7 +50,6 @@ INSTALLED_APPS = [
 
 if DEBUG:
     INSTALLED_APPS += [
-        'rest_framework',
         'django_extensions',  # shell_plus --ipython
         'drf_yasg',
     ]
