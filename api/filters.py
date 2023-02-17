d41483b9ef31f6794dce182c3090d752fd252b50
Roman Petrakov <development@telfia.com>
2023-02-17 14:53:35 +0300

    #124 finally + tests

--- api/filters.py
+++ api/filters.py
@@ -5,7 +5,8 @@ from catalog.models import RealEstate
 
 
 class CharFilterInFilter(filters.BaseInFilter, filters.CharFilter):
-    """Фильтр для поиска по списку значений."""
+    """Фильтр для поиска по списку значений. Значения разделяются запятыми"""
+    # Например: ?locaiton=1,2,3
 
 
 class RealEstateFilter(filters.FilterSet):
@@ -16,13 +17,23 @@ class RealEstateFilter(filters.FilterSet):
         field_name='property_type__id', lookup_expr='in'
     )
     location = CharFilterInFilter(field_name='location__id', lookup_expr='in')
-    rooms = filters.Filter(method='rooms_limiter')
+    rooms = filters.NumberFilter(method='rooms_limiter')
     is_favorited = filters.BooleanFilter(method='get_favorite')
+
+    class Meta:
+        model = RealEstate
+        fields = [
+            'category',
+            'property_type',
+            'location',
+            'rooms',
+            'is_favorited',
+        ]
 
     def get_favorite(self, queryset, name, value):
         """Возвращает избранные объекты недвижимости."""
         if value and self.request.user.is_authenticated:
-            return self.queryset.filter(
+            return queryset.filter(
                 id__in=self.request.user.favorites.all()
                 .values_list('real_estate_id', flat=True)
                 .order_by('-date_added')
@@ -36,17 +47,5 @@ class RealEstateFilter(filters.FilterSet):
         except ValueError:
             return queryset
         if 0 < value < settings.ROOMS_LIMIT:
-            queryset = self.queryset.filter(rooms=value)
-        else:
-            queryset = self.queryset.filter(rooms__gte=settings.ROOMS_LIMIT)
-        return queryset
-
-    class Meta:
-        model = RealEstate
-        fields = [
-            'category',
-            'property_type',
-            'location',
-            'rooms',
-            'is_favorited',
-        ]
+            return queryset.filter(rooms=value).distinct()
+        return queryset.filter(rooms__gte=settings.ROOMS_LIMIT).distinct()
