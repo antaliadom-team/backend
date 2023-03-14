7dacef98ee8cfa1089b5977f3955f981e47055a1
Maystrenko Dmitriy <kultmet@gmail.com>
2023-03-14 16:30:52 +0300

    Добавил возможность сменить пароль по ссылки из email

--- antalia_project/settings.py
+++ antalia_project/settings.py
@@ -87,20 +87,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'antalia_project.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.sqlite3'),
-#         'NAME': os.getenv('POSTGRES_DB', default=BASE_DIR / 'db.sqlite3'),
-#         'USER': os.getenv('POSTGRES_USER', default='postgres'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-#         'HOST': os.getenv('DB_HOST', default='localhost'),
-#         'PORT': os.getenv('DB_PORT', default='5432'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.sqlite3'),
+        'NAME': os.getenv('POSTGRES_DB', default=BASE_DIR / 'db.sqlite3'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='localhost'),
+        'PORT': os.getenv('DB_PORT', default='5432'),
     }
 }
 
