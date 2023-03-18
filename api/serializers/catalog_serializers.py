8712983a1e394b1f07f266798f248fa94895f723
Roman Petrakov <development@telfia.com>
2023-03-18 23:24:11 +0300

    adds order, api tests and fixes

--- api/serializers/catalog_serializers.py
+++ api/serializers/catalog_serializers.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core import validators
 from rest_framework import fields, serializers
 
 from api.validators import regex_check_number, validate_name
@@ -50,24 +51,20 @@ class CommonOrderSerializer(serializers.ModelSerializer):
         )
         model = Order
 
-    def __init__(self, instance=None, data=None, **kwargs):
-        if data and 'context' in kwargs:
-            user = kwargs['context']['request'].user
-            if user.is_authenticated:
-                data['first_name'] = user.first_name
-                data['last_name'] = user.last_name
-                data['phone'] = user.phone
-                data['email'] = user.email
-
-        super().__init__(instance=instance, data=data, **kwargs)
-
-    def validate_agreement(self, value):
+    @staticmethod
+    def validate_agreement(value):
         if not value:
             raise serializers.ValidationError('Вы должны принять соглашение.')
         return value
 
     def to_representation(self, instance):
         data = super().to_representation(instance=instance)
+        user = self.context['request'].user
+        if user.is_authenticated:
+            data['first_name'] = user.first_name
+            data['last_name'] = user.last_name
+            data['phone'] = user.phone
+            data['email'] = user.email
         if (
             hasattr(instance, 'category')
             and instance.get_category() is not None
@@ -85,7 +82,8 @@ class CommonOrderSerializer(serializers.ModelSerializer):
             data['property_type'] = instance.get_property_type()
         if hasattr(instance, 'comment') and instance.comment is None:
             data['comment'] = ''
-        data['rooms'] = instance.get_rooms()
+        if hasattr(instance, 'rooms') and instance.rooms is not None:
+            data['rooms'] = instance.get_rooms()
         return data
 
 
@@ -99,7 +97,15 @@ class OrderSerializer(CommonOrderSerializer):
     property_type = serializers.PrimaryKeyRelatedField(
         many=True, queryset=PropertyType.objects.all()
     )
-    rooms = serializers.ListSerializer(child=serializers.IntegerField())
+    rooms = serializers.ListField(
+        child=serializers.IntegerField(
+            validators=[
+                validators.MinValueValidator(1),
+                validators.MaxValueValidator(4),
+            ]
+        ),
+        required=True,
+    )
 
     class Meta(CommonOrderSerializer.Meta):
         fields = CommonOrderSerializer.Meta.fields + (
@@ -108,19 +114,6 @@ class OrderSerializer(CommonOrderSerializer):
             'property_type',
             'rooms',
         )
-
-    @staticmethod
-    def validate_rooms(value):
-        for room in value:
-            if not isinstance(room, int):
-                raise serializers.ValidationError(
-                    'Все значения должны быть числами.'
-                )
-            if room > 4:
-                raise serializers.ValidationError(
-                    'Число комнат не может быть больше 4.'
-                )
-        return value
 
     @staticmethod
     def bulk_create_for_order(objects, order, field, model):
---
+++ tests/test_orders.py
@@ -0,0 +1,127 @@
+import pytest
+
+from tests.common import APITestBase
+
+
+@pytest.mark.django_db(transaction=True)
+class TestOrders(APITestBase):
+    """Test orders API endpoint"""
+
+    def test_anonymous_common_order(
+        self, mocker, client, location, category1, property_type_apartment
+    ):
+        """Test anonymous orders"""
+        mock_send_order_emails = mocker.patch(
+            'api.views.catalog_views.send_order_emails'
+        )
+        url = self.urls['order_general']
+        data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'phone': '1234567890',
+            'email': 'test@example.com',
+            'location': [location.id],
+            'category': [category1.id],
+            'property_type': [property_type_apartment.id],
+            'agreement': True,
+            'comment': 'Test comment',
+            'rooms': [1, 2, 3],
+        }
+        response = client.post(url, data=data)
+        self.assert_status_code(201, response, url=url)
+        # assert that send_order_emails was called with the correct arguments
+        mock_send_order_emails.apply_async.assert_called_once_with(
+            kwargs={'data': response.data, 'user_id': None}, countdown=5
+        )
+
+    def test_common_order_with_empty_fields(self, mocker, client):
+        """Test anonymous orders with empty location, category, type"""
+        mocker.patch('api.views.catalog_views.send_order_emails')
+        url = self.urls['order_general']
+        data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'phone': '1234567890',
+            'email': 'test@example.com',
+            'location': [],
+            'category': [],
+            'property_type': [],
+            'agreement': True,
+            'comment': 'Test comment',
+            'rooms': [1, 2, 3],
+        }
+        response = client.post(url, data=data)
+        self.assert_status_code(201, response, url=url)
+
+    def test_common_order_wrong_rooms(
+        self, mocker, client, location, category1, property_type_apartment
+    ):
+        """Test anonymous orders with empty rooms"""
+        mocker.patch('api.views.catalog_views.send_order_emails')
+        url = self.urls['order_general']
+        data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'phone': '1234567890',
+            'email': 'test@example.com',
+            'location': [location.id],
+            'category': [category1.id],
+            'property_type': [property_type_apartment.id],
+            'agreement': True,
+            'comment': 'Test comment',
+            'rooms': [],
+        }
+        response = client.post(url, data=data)
+        self.assert_status_code(400, response, url=url)
+
+    def test_common_order_wrong_rooms_5(
+        self, mocker, client, location, category1, property_type_apartment
+    ):
+        """Test anonymous orders with room more than 4"""
+        mocker.patch('api.views.catalog_views.send_order_emails')
+        url = self.urls['order_general']
+        data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'phone': '1234567890',
+            'email': 'test@example.com',
+            'location': [location.id],
+            'category': [category1.id],
+            'property_type': [property_type_apartment.id],
+            'agreement': True,
+            'comment': 'Test comment',
+            'rooms': [1, 2, 3, 5],
+        }
+        response = client.post(url, data=data)
+        self.assert_status_code(400, response, url=url)
+
+    def test_user_common_order(
+        self,
+        mocker,
+        user,
+        user_client,
+        location,
+        category1,
+        property_type_apartment,
+    ):
+        """Test anonymous orders"""
+        mocker.patch('api.views.catalog_views.send_order_emails')
+        url = self.urls['order_general']
+        data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'phone': '1234567890',
+            'email': 'test@example.com',
+            'location': [location.id],
+            'category': [category1.id],
+            'property_type': [property_type_apartment.id],
+            'agreement': True,
+            'comment': 'Test comment',
+            'rooms': [1, 2, 3],
+        }
+        response = user_client.post(url, data=data)
+        self.assert_status_code(201, response, url=url)
+        assert response.data['first_name'] == user.first_name
+        assert response.data['last_name'] == user.last_name
+        assert response.data['phone'] == user.phone
+        assert response.data['email'] == user.email
